b9c51d797d9712071369ec98ad99aaa2758a5473
Madhur1707 <madhurpathak000@gmail.com>
2024-10-02 19:44:33 +0530

    custom hook for fetching data

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,22 +1,42 @@
 import { getJobs } from "@/api/apiJobs";
-import { useSession } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
 
-  const fetchJobs = async () => {
-    const supabaseAccessToken = await session.getToken({
-      template: "supabase",
-    });
-    const data = await getJobs(supabaseAccessToken);
-    console.log(data);
-  };
+  const { isLoaded } = useUser();
+
+  const {
+    fn: fnJobs,
+    data: dataJobs,
+    loadingZ: loadingJobs,
+  } = useFetch(getJobs, { location, searchQuery, company_id });
+
+  console.log(dataJobs);
 
   useEffect(() => {
-    fetchJobs();
-  }, []);
+    if (isLoaded) fnJobs();
+  }, [isLoaded, location, company_id, searchQuery]);
 
-  return <div>JobListing</div>;
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+      {loadingJobs && (
+        <BarLoader className="m" width={"100%"} color="#36d7b7" />
+      )}
+    </div>
+  );
 };
 
 export default JobListing;
