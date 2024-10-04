558a8bb9b1a73a3a6cd51d4e9348879b8b9cb22b
Madhur1707 <madhurpathak000@gmail.com>
2024-10-04 09:25:37 +0530

    saved Hobs

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,28 +1,66 @@
 import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token, { alreadySaved }, saveData) {
+// Fetch Jobs
+
+export async function getJobs(token, { location, company_id, searchQuery }) {
+  const supabase = await supabaseClient(token);
+
+  let query = supabase
+    .from("jobs")
+    .select(
+      "*, saved_jobs!saved_jobs_job_id_fkey(id), company: companies(name,logo_url)"
+    );
+
+  if (location) {
+    query = query.eq("location", location);
+  }
+
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
+
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Error fetching Jobs:", error);
+    return null;
+  }
+
+  return data;
+}
+
+// Add / Remove Saved Job
+
+export async function saveJob(token, { alreadySaved }, saveData) {
   const supabase = await supabaseClient(token);
 
   if (alreadySaved) {
+    // If the job is already saved, remove it
     const { data, error: deleteError } = await supabase
       .from("saved_jobs")
       .delete()
-      .eq("job_id", saveData.job_id);
+      .eq("job_id", saveData.job_id)
+      .select("*, jobs!saved_jobs_job_id_fkey(*)");
 
     if (deleteError) {
-      console.log("Error deleting saved jobs", deleteError);
+      console.error("Error removing saved job:", deleteError);
       return null;
     }
 
     return data;
   } else {
+    // If the job is not saved, add it to saved jobs
     const { data, error: insertError } = await supabase
       .from("saved_jobs")
       .insert([saveData])
-      .select();
+      .select("*, jobs!saved_jobs_job_id_fkey(*)");
 
     if (insertError) {
-      console.log("Error fetching jobs", insertError);
+      console.error("Error saving job:", insertError);
       return null;
     }
 
